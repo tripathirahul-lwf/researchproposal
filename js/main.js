bf4cee64de9ed1e9400731c61670c31e84ddcece
Rahul Tripathi <rahullearnwithfraternity@gmail.com>
2023-09-30 11:45:24 +0530

    developed content seciton

--- js/main.js
+++ js/main.js
@@ -1,6 +1,6 @@
 
 (function () {
-    
+
 
 
     /*=====================================
@@ -52,21 +52,52 @@
     // window.document.addEventListener('scroll', onScroll);
 
     // for menu scroll 
-    var pageLink = document.querySelectorAll('.page-scroll');
+    "use strict";
 
-    pageLink.forEach(elem => {
-        elem.addEventListener('click', e => {
-            e.preventDefault();
-            document.querySelector(elem.getAttribute('href')).scrollIntoView({
-                behavior: 'smooth',
-                offsetTop: 1 - 60,
-            });
+    const navEl = document.querySelector("#nav");
+    const mainEl = document.querySelector("#main");
+
+    const STATE = {
+        currentTab: null
+    };
+
+    const targetsContainer = document.querySelector(".targets");
+    const triggers = Array.from(document.querySelectorAll(".trigger"));
+    const targets = [];
+    const triggerSpec = document.querySelector(".trigger-spec");
+    triggerSpec.style.width = `${100 / triggers.length}%`;
+
+    triggers.forEach((trigger, ind) => {
+        targets.push(document.querySelector(trigger.dataset.target));
+        trigger.addEventListener("click", () => {
+            STATE.currentTab = deactivateTab(STATE.currentTab);
+            STATE.currentTab = activateTab(ind);
         });
     });
 
-    // WOW active
+    STATE.currentTab = activateTab(0);
 
-  
+    function activateTab(ind) {
+        if (ind == null) return ind;
+        const trigger = triggers[ind];
+        trigger.classList.add("active");
+        const target = targets[ind];
+        target.classList.add("active");
+        targetsContainer.style.transform = `translateX(-${ind}00%)`;
+        triggerSpec.style.transform = `translateX(${ind}00%)`;
+        return ind;
+    }
+
+    function deactivateTab(ind) {
+        if (ind == null) return ind;
+        const trigger = triggers[ind];
+        trigger.classList.remove("active");
+        const target = targets[ind];
+        target.classList.remove("active");
+        return null;
+    }
+
+
 
     //===== mobile-menu-btn
     let navbarToggler = document.querySelector(".mobile-menu-btn");
